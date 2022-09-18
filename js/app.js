ab186f4692c2be48b1ce21ca0667b02ce850443b
Isaiah Mitchell <Isaiah2417@yahoo.com>
2022-09-18 14:45:20 -0500

    Added soultion 4

--- js/app.js
+++ js/app.js
@@ -7,9 +7,13 @@ Write a function called sum() that takes in two numbers as arguments and then re
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 // Write your code here
-function sum(a, b) { //eslint-disable-line
-  let mySum = a+b;
-  let myStr = `The sum of ${a} and ${b} is ${mySum}.`;
+
+
+let solution = sum(45,2);
+
+function sum(argument1, argument2) { //eslint-disable-line
+  let mySum = argument1+argument2;
+  let myStr = `The sum of ${argument1} and ${argument2} is ${mySum}.`;
   return [mySum, myStr];
 }
 
@@ -65,7 +69,7 @@ let myStr2 = `The product of ${a} and ${b} and ${c} is ${pdtAbc[0]}.`;
 
 return [sumAbc[0], pdtAbc[0], myStr, myStr2];
 }
-console.log(sumAndMultiply(4,7,5));
+
 
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
@@ -85,14 +89,17 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 let testArray = [2, 3, 4]; //eslint-disable-line
-
 function sumArray(sumArr) { //eslint-disable-line
-
+  let summ = sum(sumArr[0],sumArr[1]);
+  let summ1 = sum(summ[0], sumArr[2]);
+console.log(summ1)
+  let solStr = `${sumArr} was passed in as an array of numbers, and ${summ1[0]} is their sum.`;
+return [summ1[0], solStr];
 }
-
+console.log(sumArray(testArray));
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
